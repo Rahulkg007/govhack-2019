4a7008d4c8819bf8045ea1d0e6c8aa7528567adc
Rahul Gupta <rahulkg007@gmail.com>
2019-09-07 11:44:11 +1000

    Added R analysis headers

---
+++ src/Analysis.R
@@ -0,0 +1,9 @@
+# @title GovHack Analysis
+# @author Rahul K Gupta
+# @date 7-Sep-2019
+
+# Import Libraries
+require(tidyverse)
+
+# Set directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
