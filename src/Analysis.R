e318d0074ade4a776a067041302769141cb49a94
Rahul Gupta <rahulkg007@gmail.com>
2019-09-07 15:25:17 +1000

    Sports and recreational added

--- src/Analysis.R
+++ src/Analysis.R
@@ -7,3 +7,17 @@ require(tidyverse)
 
 # Set directory
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Sports and Recreational Activities
+
+sports = read_csv("../data/SportandRec.csv")
+sports = sports %>% filter(Postcode %in% c(3011, 3056))
+sports$SuburbTown = factor(sports$SuburbTown, levels = c("BRUNSWICK","FOOTSCRAY"), labels = c("Brunswick","Footscray"))
+
+sportsAggregated = sports %>% 
+  group_by(SuburbTown) %>% 
+  summarise(count = n())
+
+ggplot(sports, aes(SuburbTown)) + geom_bar(fill="#2c7fb8") + 
+  theme_minimal() + coord_flip() +
+  xlab('')
